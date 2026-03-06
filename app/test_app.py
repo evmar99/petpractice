4192e86842dca3e200b4f05711dbe9c935ecf34e
evmar99 <evmarin93@gmail.com>
2026-03-06 15:47:11 +0200

    feat: Added lint and pytest

---
+++ app/test_app.py
@@ -0,0 +1,16 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_health(client):
+    response = client.get('/health')
+    assert response.status_code == 200
+
+def test_get_tasks(client):
+    response = client.get('/tasks')
+    assert response.status_code == 200
